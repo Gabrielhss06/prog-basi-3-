61a778240780b2b20dc08a1e250d14b6015d752d
Gabrielhss06 <161782014+Gabrielhss06@users.noreply.github.com>
2024-09-27 00:40:41 -0300

    att-1-abstracao

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-            public static void main(String[] args) {
+    public static void main(String[] args) {
+        // Criando alguns alunos
+        Aluno aluno1 = new Aluno("João Silva", 17, 70.5, 1.75);
+        Aluno aluno2 = new Aluno("Maria Souza", 19, 60.0, 1.68);
+        Aluno aluno3 = new Aluno("Carlos Almeida", 16, 80.3, 1.80);
+        Aluno aluno4 = new Aluno("Ana Costa", 21, 55.2, 1.65);
 
-                Universidade princeton = new Universidade(" Princeton (Nova Jersey - Estados Unidos da América)");
-                Universidade cambridge = new Universidade("Cambridge (Inglaterra)");
-
-
-                Pessoa einstein = new Pessoa("Albert Einstein", "14/03/1879", princeton);
-                Pessoa newton = new Pessoa("Isaac Newton", "04/01/1643", cambridge);
-
-                einstein.dizerOndeTrabalha();
-                newton.dizerOndeTrabalha();
-            }
-        }
+        // Exibindo informações dos alunos
+        System.out.println(aluno1);
+        System.out.println();
+        System.out.println(aluno2);
+        System.out.println();
+        System.out.println(aluno3);
+        System.out.println();
+        System.out.println(aluno4);
+    }
+}
