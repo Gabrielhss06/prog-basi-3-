ee3187480324e99066f6af7d95bb807e7e995643
Gabrielhss06 <161782014+Gabrielhss06@users.noreply.github.com>
2024-09-27 00:45:42 -0300

    att-2-abstracao

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,25 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Criando alguns alunos
-        Aluno aluno1 = new Aluno("João Silva", 17, 70.5, 1.75);
-        Aluno aluno2 = new Aluno("Maria Souza", 19, 60.0, 1.68);
-        Aluno aluno3 = new Aluno("Carlos Almeida", 16, 80.3, 1.80);
-        Aluno aluno4 = new Aluno("Ana Costa", 21, 55.2, 1.65);
+        // Criando uma conta com saldo inicial
+        Conta conta1 = new Conta("12345", "João Silva", 500.00);
 
-        // Exibindo informações dos alunos
-        System.out.println(aluno1);
+
+        System.out.println(conta1);
         System.out.println();
-        System.out.println(aluno2);
+
+
+        conta1.depositar(200.00);
+        System.out.println("Saldo atual: R$ " + conta1.getSaldo());
         System.out.println();
-        System.out.println(aluno3);
+
+
+        conta1.sacar(800.00);
+        System.out.println("Saldo atual: R$ " + conta1.getSaldo());
         System.out.println();
-        System.out.println(aluno4);
+
+
+        conta1.sacar(150.00);
+        System.out.println("Saldo atual: R$ " + conta1.getSaldo());
     }
 }
